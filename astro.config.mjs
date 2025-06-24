e89d49b08d042f8abc22476c9748fdf87b122a92
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-25 00:13:47 +0200

    build: add tailwindcss and preact integrations to astro config
    Add tailwindcss as a vite plugin and preact as an integration to enable their functionality in the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+import preact from '@astrojs/preact';
+
+export default defineConfig({
+	vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [preact()]
+})

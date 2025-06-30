a41570c52dbaef96c9043bea8da5bb58d89a4928
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-30 13:29:08 +0200

    feat(icons): add chevronDown icon component

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -435,6 +435,23 @@ export const Icons = {
       <path d='m18 15-6-6-6 6' />
     </svg>
   ),
+  chevronDown: (props: IconProps) => (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+      class='lucide lucide-chevron-down-icon lucide-chevron-down'
+      {...props}
+    >
+      <path d='m6 9 6 6 6-6' />
+    </svg>
+  ),
   infinity: (props: IconProps) => (
     <svg
       xmlns='http://www.w3.org/2000/svg'

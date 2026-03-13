4126a2708742824b72d2c29fa71ca2b9661c350b
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2026-03-13 16:50:19 +0100

    feat(previews): add previews module for snippet components
    - Introduced a new previews module to manage and retrieve preview components for snippets.
    - Defined an enum for preview types and created a mapping of previews to their respective components.
    - Implemented a function to fetch the appropriate preview component based on snippet data.

---
+++ src/lib/previews.ts
@@ -0,0 +1,18 @@
+import InputStateAnimation from '@/components/snippets/previews/InputStateAnimation.astro'
+import type { CollectionEntry } from 'astro:content'
+
+type PreviewComponent = typeof InputStateAnimation
+
+export enum Previews {
+  INPUT_STATE_ANIMATION = 'input-state-animation'
+}
+
+export const PREVIEWS_COMPONENTS: Record<Previews, PreviewComponent> = {
+  [Previews.INPUT_STATE_ANIMATION]: InputStateAnimation
+}
+
+export const getPreviewComponent = (
+  snippet: CollectionEntry<'snippets'>
+): PreviewComponent => {
+  return PREVIEWS_COMPONENTS[snippet.data.preview]
+}
